2299d60d3a93bff0d5a3b29e71b19d95bd540346
Robert Gannon <cyberpeaches@gmail.com>
2021-06-09 02:18:58 +0100

    Aligned expandable box into columes. Removed closed stores. Added average stock level. FIltered duplicate products, and products with no stock.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+//is the estimate between the selected dates
 const isInDate = (estimate) => {
     const estimateDate = Date.parse(estimate.created_at);
     if (estimateDate <= dateTo && estimateDate >= dateFrom) {
@@ -7,19 +8,27 @@ const isInDate = (estimate) => {
     }
 }
 
-const fetching = () => {
-    document.getElementById("loader_donut").classList.toggle("pswp__preloader__donut");
+//Finds index on part quantity array for selected store
+const selectedStoreQuantity = (quantities) => {
+    return quantities.findIndex(item => {
+      if (item.location_id == storeID){
+        return true;
+      }
+      return false;
+    });
+  } 
+
+//If estimate matches selected parameters then it pushes its id to an array 
+const getMatch = (item) => {
+    if (item.location_id === storeID && isInDate(item) ){
+        estimateIDs.push(item.id);
+    }
 }
 
+//Gets ids from estimates with selected parameters using getMatch()
 const getIDs = async (estimates) => {
     let estimatesArr = estimates.estimates;
     let lastEstimateOnPage = estimatesArr[estimatesArr.length - 1];
-
-    const getMatch = (item) => {
-        if (item.location_id === storeID && isInDate(item) ){
-            estimateIDs.push(item.id);
-        }
-    }
 
     let count = 2;
     while (isInDate(lastEstimateOnPage)){
@@ -35,4 +44,8 @@ const getIDs = async (estimates) => {
 
 const detailExpand = (num) => {
     document.getElementById(`details${num}`).classList.toggle("expand");
-  }
+}
+
+const fetching = () => {
+    document.getElementById("loader_donut").classList.toggle("pswp__preloader__donut");
+}
